5165f9b92cb8f97a6910e0c3119e8bcb6e6fdf2c
Jan <janchris80>
2020-07-16 03:22:16 -0700

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,10 @@ const About = () => {
       <h1>About this app</h1>
       <p>app to search github users</p>
       <p>version: 1.0.0</p>
+      <p>Created: 7/16/2020</p>
+      <p>
+        Developer: <a href='https://github.com/janchris80'>Jan Chris</a>
+      </p>
     </Fragment>
   );
 };
